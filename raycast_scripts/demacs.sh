42303f06b24b9cd950a3026d32c65bb05b5e82bc
mrunhap <liubolovelife@gmail.com>
2021-06-20 22:01:05 +0800

    add raycast scripts for macos

---
+++ raycast_scripts/demacs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title DEmacs
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Dumped Emacs
+# @raycast.author 404cn
+# @raycast.authorURL htts://github.com/404cn
+
+~/.config/emacs/dmacs
