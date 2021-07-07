cd072abe1ba1e7266170ea5cf6c6b37c8f52d8ee
mrunhap <liubolovelife@gmail.com>
2021-07-07 21:57:08 +0800

    update tmux config

--- install_brew.sh
+++ install_brew.sh
@@ -80,6 +80,7 @@ apps=(
 
 # command line apps
 formulae_apps=(
+    clipper # access for local and remote tmux sessions
     miniconda # python virtual env 
     pgcli # postgress command line tool
     litecli # sqllite command line tool
