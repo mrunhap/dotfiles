746f964f90ef1ccdac0441908488b8dc18fec68e
mrunhap <liubolovelife@gmail.com>
2021-09-16 17:25:28 +0800

    delete nix config

--- .nixpkgs/darwin-configuration.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
-
-  # ...
-}
--- config/nixpkgs/darwin/configuration.nix
+++
@@ -1,5 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  nix.binaryCaches = [ "https://mirrors.ustc.edu.cn/nix-channels/store" "https://cache.nixos.org/" ];
-}
