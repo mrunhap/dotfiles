08927a8867e69fcf89979b78d2f154f5a3394449
mrunhap <liubolovelife@gmail.com>
2021-09-18 16:28:19 +0800

    update

---
+++ scripts/install_arch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# First install arch via archinstall and choose i3-gaps.
+# Add additional packages like git vim.
+# Use networkmanager and set timezone to Asia/Shanghai
+# Replace display manager to ly
+#git clone https://github.com/nullgemm/ly.git
+#make github
+#make
+#sudo make install
+#sudo systemctl enable ly
+# After done these, run this script.
