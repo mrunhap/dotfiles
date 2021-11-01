b7dbdb81c50db7760ef1596db5ba6049ba467ec0
mrunhap <liubolovelife@gmail.com>
2021-11-01 19:57:02 +0800

    update fish config, set correct env var use set

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,30 +5,37 @@ end
 set -g fish_greeting
 
 # envioronment variable {
-set LANG "en_US.UTF-8"
-set EDITOR vim
+set -gx LANG "en_US.UTF-8"
+set -gx EDITOR vim
 
 # sdcv
-set STARDICT_DATA_DIR $HOME/.sdcv-dict
+set -gx STARDICT_DATA_DIR $HOME/.sdcv-dict
 
 # golang
-set GOPROXY https://goproxy.cn
-set GO111MODULE on
-set GOPATH $HOME/go
+set -gx GOPROXY https://goproxy.cn
+set -gx GO111MODULE on
+set -gx GOPATH $HOME/go
 fish_add_path $GOPATH/bin
 
 # for osx tui emacs paste non-ascii mess-up problem
-set LC_ALL en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
 
 # gnu global && emacs citre-global
 # Set this to save gtags database to GTAGSOBJDIRPREFIX/<project-root>.  This
 # requires --objdir option in gtags command line, see citre-global-gtags-args.
-set GTAGSOBJDIRPREFIX ~/.cache/gtags/
+set -gx GTAGSOBJDIRPREFIX ~/.cache/gtags/
 # Make sure you use the path to the default config file on your machine.  This
 # file contains the definition for Pygments plugin parser.
-set GTAGSCONF ~/.globalrc
+set -gx GTAGSCONF ~/.globalrc
 # Use gtags built in parse for native support language and others use pygments.
-set GTAGSLABEL native-pygments
+set -gx GTAGSLABEL native-pygments
+
+# fzf
+set -g FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git || git ls-tree -r --name-only HEAD || rg --hidden --files || find ."
+set -g FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -g FZF_CTRL_T_OPTS "--preview '(bat --style=numbers --color=always {} || cat {} || tree -NC {}) 2> /dev/null | head -200'"
+set -g FZF_CTRL_R_OPTS "--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview' --exact"
+set -g FZF_ALT_C_OPTS "--preview 'tree -NC {} | head -200'"
 
 #}
 
