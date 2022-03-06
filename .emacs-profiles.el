33c85c662be1bf352f4bfaf04b95aac7153f73f2
mrunhap <liubolovelife@gmail.com>
2022-03-06 14:56:34 +0800

    add emacs profiles
    Former-commit-id: 9ea816a55e247b1713e3bc6f06ddd64447ac3aa9

---
+++ .emacs-profiles.el
@@ -0,0 +1,24 @@
+(("default" . ((user-emacs-directory . "~/.config/emacs")))
+ ("cjohansen" . ((user-emacs-directory . "~/repo/emacsconf/cjohansen")))
+ ("lazycat" . ((user-emacs-directory . "~/repo/emacsconf/lazycat")))
+ ("tecosaur" . ((user-emacs-directory . "~/repo/emacsconf/tecosaur")))
+ ("toki" . ((user-emacs-directory . "~/repo/emacsconf/toki")))
+ ("redguardtoo" . ((user-emacs-directory . "~/repo/emacsconf/redguardtoo")))
+ ("dog" . ((user-emacs-directory . "~/repo/emacsconf/dog")))
+ ("snug" . ((user-emacs-directory . "~/repo/emacsconf/snug")))
+ ("centaur" . ((user-emacs-directory . "~/repo/emacsconf/centaur")))
+ ("lunary" . ((user-emacs-directory . "~/repo/emacsconf/lunary")))
+ ("purcell" . ((user-emacs-directory . "~/repo/emacsconf/purcell")))
+ ("prelude" . ((user-emacs-directory . "~/repo/emacsconf/prelude"))))
+
+;; git clone https://github.com/purcell/emacs.d.git ~/repo/emacsconf/purcell
+;; git clone https://github.com/redguardtoo/emacs.d.git ~/repo/emacsconf/redguardtoo
+;; git clone https://github.com/seagle0128/.emacs.d.git  ~/repo/emacsconf/centaur
+;; git clone https://github.com/bbatsov/prelude  ~/repo/emacsconf/prelude
+;; git clone https://github.com/tecosaur/emacs-config.git  ~/repo/emacsconf/tecosaur
+;; git clone https://github.com/DogLooksGood/meomacs.git ~/repo/emacsconf/dog
+;; git clone https://github.com/ogdenwebb/snug-emacs.git  ~/repo/emacsconf/snug
+;; git clone https://github.com/casouri/lunarymacs.git ~/repo/emacsconf/lunary
+;; git clone https://github.com/AmaiKinono/Tokimacs.git ~/repo/emacsconf/toki
+;; git clone https://github.com/manateelazycat/lazycat-emacs.git ~/repo/emacsconf/lazycat
+;; git clone https://github.com/cjohansen/.emacs.d.git ~/repo/emacsconf/cjohansen
