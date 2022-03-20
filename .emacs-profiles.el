059c1d47edc08ec487e0736d6e0ab182f427911a
mrunhap <liubolovelife@gmail.com>
2022-03-20 21:39:55 +0800

    replace repo folder with Developer
    Former-commit-id: 97e3f1c47a09d0a8fadbe5cd24394cb736f802bf

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,30 +1,31 @@
 (("default" . ((user-emacs-directory . "~/.config/emacs")))
- ("aeghn" . ((user-emacs-directory . "~/repo/emacsconf/aeghn")))
- ("cjohansen" . ((user-emacs-directory . "~/repo/emacsconf/cjohansen")))
- ("lazycat" . ((user-emacs-directory . "~/repo/emacsconf/lazycat")))
- ("tecosaur" . ((user-emacs-directory . "~/repo/emacsconf/tecosaur")))
- ("toki" . ((user-emacs-directory . "~/repo/emacsconf/toki")))
- ("redguardtoo" . ((user-emacs-directory . "~/repo/emacsconf/redguardtoo")))
- ("dog" . ((user-emacs-directory . "~/repo/emacsconf/dog")))
- ("snug" . ((user-emacs-directory . "~/repo/emacsconf/snug")))
- ("centaur" . ((user-emacs-directory . "~/repo/emacsconf/centaur")))
- ("lunary" . ((user-emacs-directory . "~/repo/emacsconf/lunary")))
- ("purcell" . ((user-emacs-directory . "~/repo/emacsconf/purcell")))
- ("prelude" . ((user-emacs-directory . "~/repo/emacsconf/prelude"))))
+ ("aeghn" . ((user-emacs-directory . "~/Developer/emacsconf/aeghn")))
+ ("mengmeng" . ((user-emacs-directory . "~/Developer/emacsconf/mengmeng")))
+ ("cjohansen" . ((user-emacs-directory . "~/Developer/emacsconf/cjohansen")))
+ ("lazycat" . ((user-emacs-directory . "~/Developer/emacsconf/lazycat")))
+ ("tecosaur" . ((user-emacs-directory . "~/Developer/emacsconf/tecosaur")))
+ ("toki" . ((user-emacs-directory . "~/Developer/emacsconf/toki")))
+ ("redguardtoo" . ((user-emacs-directory . "~/Developer/emacsconf/redguardtoo")))
+ ("dog" . ((user-emacs-directory . "~/Developer/emacsconf/dog")))
+ ("snug" . ((user-emacs-directory . "~/Developer/emacsconf/snug")))
+ ("centaur" . ((user-emacs-directory . "~/Developer/emacsconf/centaur")))
+ ("lunary" . ((user-emacs-directory . "~/Developer/emacsconf/lunary")))
+ ("purcell" . ((user-emacs-directory . "~/Developer/emacsconf/purcell")))
+ ("prelude" . ((user-emacs-directory . "~/Developer/emacsconf/prelude"))))
 
 ;; git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
-;; git clone https://github.com/purcell/emacs.d.git ~/repo/emacsconf/purcell
-;; git clone https://github.com/redguardtoo/emacs.d.git ~/repo/emacsconf/redguardtoo
-;; git clone https://github.com/seagle0128/.emacs.d.git  ~/repo/emacsconf/centaur
-;; git clone https://github.com/bbatsov/prelude  ~/repo/emacsconf/prelude
-;; git clone https://github.com/tecosaur/emacs-config.git  ~/repo/emacsconf/tecosaur
-;; git clone https://github.com/DogLooksGood/meomacs.git ~/repo/emacsconf/dog
-;; git clone https://github.com/ogdenwebb/snug-emacs.git  ~/repo/emacsconf/snug
-;; git clone https://github.com/casouri/lunarymacs.git ~/repo/emacsconf/lunary
-;; git clone https://github.com/AmaiKinono/Tokimacs.git ~/repo/emacsconf/toki
-;; git clone https://github.com/manateelazycat/lazycat-emacs.git ~/repo/emacsconf/lazycat
-;; git clone https://github.com/cjohansen/.emacs.d.git ~/repo/emacsconf/cjohansen
-;; git clone https://github.com/aeght/emacs.d.git ~/repo/emacsconf/aeghn
+;; git clone https://github.com/purcell/emacs.d.git ~/Developer/emacsconf/purcell
+;; git clone https://github.com/redguardtoo/emacs.d.git ~/Developer/emacsconf/redguardtoo
+;; git clone https://github.com/seagle0128/.emacs.d.git  ~/Developer/emacsconf/centaur
+;; git clone https://github.com/bbatsov/prelude  ~/Developer/emacsconf/prelude
+;; git clone https://github.com/tecosaur/emacs-config.git  ~/Developer/emacsconf/tecosaur
+;; git clone https://github.com/DogLooksGood/meomacs.git ~/Developer/emacsconf/dog
+;; git clone https://github.com/ogdenwebb/snug-emacs.git  ~/Developer/emacsconf/snug
+;; git clone https://github.com/casouri/lunarymacs.git ~/Developer/emacsconf/lunary
+;; git clone https://github.com/AmaiKinono/Tokimacs.git ~/Developer/emacsconf/toki
+;; git clone https://github.com/manateelazycat/lazycat-emacs.git ~/Developer/emacsconf/lazycat
+;; git clone https://github.com/cjohansen/.emacs.d.git ~/Developer/emacsconf/cjohansen
+;; git clone https://github.com/aeght/emacs.d.git ~/Developer/emacsconf/aeghn
+;; git clone https://github.com/condy0919/.emacs.d.git ~/Developer/emacsconf/mengmeng
 
-;; git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
 ;; emacs --with-profile default
