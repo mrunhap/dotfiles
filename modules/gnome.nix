4f3d03d3bccbcca65b800b08fff48723ddd35607
mrunhap <liubolovelife@gmail.com>
2023-01-11 00:02:46 +0800

    switch to nix flake
    Former-commit-id: 42b4dae29b4452f3780ea1ea1794c5b795c8f493 [formerly 88ce5e66469efb217ed5fddea419a9fab0b9daa7]
    Former-commit-id: 0e0254599ec35b913bf726e56813719a2c096a30

---
+++ modules/gnome.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ...}:
+
+{
+  home.packages = with pkgs; [
+    gnome.gnome-tweaks
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.appindicator
+    gnomeExtensions.gsconnect
+    gnomeExtensions.caffeine
+    gnomeExtensions.pop-shell
+  ];
+
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "palenight";
+      package = pkgs.palenight-theme;
+    };
+
+    cursorTheme = {
+      name = "Numix-Cursor";
+      package = pkgs.numix-cursor-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  dconf.settings = {
+
+  };
+
+  home.sessionVariables.GTK_THEME = "palenight";
+}
