03c3c27123aeede0df4eb2a618594a7f5823cd0e
mrunhap <liubolovelife@gmail.com>
2023-04-26 17:25:44 +0800

    add texlive small

--- modules/home/tui.nix
+++ modules/home/tui.nix
@@ -43,7 +43,7 @@ in
 
   home.sessionVariables = {
     LANG     = "en_US.UTF-8";
-    EDITOR   = "emacs -nw";
+    EDITOR   = "emacsclient -a '' -nc";
     TERM     = "xterm-256color";
     LC_CTYPE = "en_US.UTF-8";
     NIX_PATH = "$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels";
