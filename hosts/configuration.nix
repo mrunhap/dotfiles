9e107b304b4610a93f8b3d9e6c217348082ea256
mrunhap <liubolovelife@gmail.com>
2023-06-25 10:52:53 +0800

    use cache, add fhs

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -8,6 +8,25 @@
 
   environment.systemPackages = with pkgs; [
     git
+
+    # create a fhs environment by command `fhs`, so we can run non-nixos packages in nixos!
+    (pkgs.buildFHSUserEnv (base // {
+      name = "fhs";
+      targetPkgs = pkgs: (
+        # pkgs.buildFHSUserEnv 只提供一个最小的 FHS 环境，缺少很多常用软件所必须的基础包
+        # 所以直接使用它很可能会报错
+        #
+        # pkgs.appimageTools 提供了大多数程序常用的基础包，所以我们可以直接用它来补充
+        (pkgs.appimageTools.defaultFhsEnvArgs.targetPkgs pkgs) ++ with pkgs; [
+          pkg-config
+          ncurses
+          # 如果你的 FHS 程序还有其他依赖，把它们添加在这里
+        ]
+      );
+      profile = "export FHS=1";
+      runScript = "bash";
+      extraOutputsToInstall = ["dev"];
+    }))
   ];
 
   nix = {                                   # Nix Package Manager settings
