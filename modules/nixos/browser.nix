14889e92a4fed3326658ce36c67252b3bd390e39
mrunhap <liubolovelife@gmail.com>
2023-12-14 23:29:22 +0800

    update nixos browser

--- modules/nixos/browser.nix
+++ modules/nixos/browser.nix
@@ -10,28 +10,9 @@ let
   # DL link.
   # 6. Or go to about:debugging#/runtime/this-firefox
   extensions = {
-    # subscribe rss
-    "i@diygod.me" = "rsshub-radar";
-    # black list
-    "@ublacklist" = "ublacklist";
-    # scripts
-    "firefox@tampermonkey.net" = "tampermonkey";
-    # Content blocker.
-    "uBlock0@raymondhill.net" = "ublock-origin";
-    # Mark (skip) sponsored & other video segments on YouTube.
-    "sponsorBlocker@ajay.app" = "sponsorblock";
-    # subscribe rss
+    # only need these two extensions, others will sync after login.
     "treestyletab@piro.sakura.ne.jp" = "tree-style-tab";
-    # 划词翻译
-    "{0982b844-4f35-48b7-9811-6832d916f21c}" = "hcfy";
-    # CAPTCHA Solver
-    "{2f67aecb-5dac-4f76-9378-0ac4f2bedc9c}" = "noptcha";
-    # bitwarden password manager
     "{446900e4-71c2-419f-a6a7-df9c091e268b}" = "bitwarden-password-manager";
-    # mouse gestures
-    "{506e023c-7f2b-40a3-8066-bc5deb40aebe}" = "gesturefy";
-    # base64 decoder
-    "{b20e4f00-ab03-4a88-90e7-4f6b6232c5a9}" = "base64-decoder";
   };
 in
 {
@@ -45,12 +26,6 @@ in
           installation_mode = "normal_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/${name}/latest.xpi";
         })
-        extensions;#  // {
-        #   installation_mode = "normal_installed";
-        #   install_url = "https://github.com/pt-plugins/PT-Plugin-Plus/releases/download/v1.6.1.2353/PT-Plugin-Plus-1.6.1.2353.xpi";
-        # };
-
-    # TODO Webextension configuration.
-    # "3rdparty".Extensions = {};
+        extensions;
   };
 }
