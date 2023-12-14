ff45f8baec1ace3f6eca83bcd18ab5d10cbf6fd9
mrunhap <liubolovelife@gmail.com>
2023-12-14 23:29:22 +0800

    update fcitx5

--- modules/home/fcitx5.nix
+++ modules/home/fcitx5.nix
@@ -9,16 +9,10 @@
 
   home.activation.removeExistingFcitx5 = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
     rm -rf ~/.config/fcitx5
-    rm -rf ~/.local/share/fcitx5/themes
   '';
   home.file.".config/fcitx5" = {
     source = ../../modules/files/fcitx5;
     recursive = true;
   };
-  home.file.".local/share/fcitx5/themes" = {
-    source = ../../modules/files/fcitx5-theme;
-    recursive = true;
-  };
-
   # TODO check rime
 }
