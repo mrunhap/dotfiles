0b5b84f55f4cb5a53a57442f361aa31f8dffd0a1
mrunhap <liubolovelife@gmail.com>
2023-12-14 23:29:22 +0800

    update ags config

--- flake.nix
+++ flake.nix
@@ -64,11 +64,11 @@
 
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "xddxdd.cachix.org-1:ay1HJyNDYmlSwj5NXQG065C8LfoqqKaTNCyzeixGjf8="
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "xddxdd.cachix.org-1:ay1HJyNDYmlSwj5NXQG065C8LfoqqKaTNCyzeixGjf8="
     ];
     trusted-users = [ "root" "liubo" "gray" ];
   };
