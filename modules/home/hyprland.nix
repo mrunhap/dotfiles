db13b429808b92157506729403e3f31fb6298eb9
mrunhap <liubolovelife@gmail.com>
2024-01-28 13:35:11 +0800

    update hyprland workspace config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -80,12 +80,12 @@
       workspace = [
         "1, monitor:DP-2, default:true"
         "2, monitor:DP-2"
-        "3, monitor:DP-3"
+        "3, monitor:DP-2"
         "4, monitor:DP-3"
-        "5, monitor:DP-2"
+        "5, monitor:DP-3"
         "6, monitor:DP-2"
-        "7, monitor:DP-3"
-        "8, monitor:DP-3"
+        "7, monitor:DP-2"
+        "8, monitor:DP-2"
         "9, monitor:HDMI-A-2"
         "10, monitor:HDMI-A-2"
       ];
