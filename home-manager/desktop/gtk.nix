9e99c92147fd7a66767f4fcb30d71fcab353ba99
mrunhap <liubolovelife@gmail.com>
2024-04-02 01:06:15 +0800

    update gtk cursor theme

--- home-manager/desktop/gtk.nix
+++ home-manager/desktop/gtk.nix
@@ -10,6 +10,13 @@
     rm -rf ~/.config/gtk-4.0
   '';
 
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 32;
+  };
+
   gtk = {
     enable = true;
 
