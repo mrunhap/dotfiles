953fab2f7d2d86ab899ef91252ce48ec4115185d
mrunhap <liubolovelife@gmail.com>
2024-04-09 15:59:24 +0800

    update

--- flake.nix
+++ flake.nix
@@ -96,12 +96,10 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
-      "https://walker.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
     ];
     trusted-users = ["root" "liubo" "gray" "mrunhap"];
   };
