4d1a3adc72e870c9dcea7ea5f0f6bf98fc099931
mrunhap <liubolovelife@gmail.com>
2024-04-25 22:13:43 +0800

    update nvidia

--- nixos/nvidia/default.nix
+++ nixos/nvidia/default.nix
@@ -15,7 +15,7 @@
   services.xserver.videoDrivers = ["nvidia"];
 
   # https://wiki.hyprland.org/Nvidia/#fixing-suspendwakeup-issues
-  # boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
 
   hardware.nvidia = {
 
@@ -26,7 +26,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
