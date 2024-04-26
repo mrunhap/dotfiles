136cf15831aafea17a394d6d32e3b89cff06a3ed
mrunhap <liubolovelife@gmail.com>
2024-04-26 14:42:35 +0800

    zsh plugin add alias-tips

--- home-manager/shell/default.nix
+++ home-manager/shell/default.nix
@@ -155,6 +155,16 @@
           file = "autopair.zsh";
         }
         {
+          name = "alias-tips";
+          src = fetchFromGitHub {
+            owner = "djui";
+            repo = "alias-tips";
+            rev = "41cb143ccc3b8cc444bf20257276cb43275f65c4";
+            sha256 = "sha256-ZFWrwcwwwSYP5d8k7Lr/hL3WKAZmgn51Q9hYL3bq9vE=";
+          };
+          file = "alias-tips.plugin.zsh";
+        }
+        {
           name = "git.plugin.zsh";
           src = fetchurl {
             url = "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh";
