d9b9494a11de044bde052201006171d3864e8f62
mrunhap <liubolovelife@gmail.com>
2024-06-12 05:26:26 +0800

    update north network

--- hosts/north/default.nix
+++ hosts/north/default.nix
@@ -12,13 +12,17 @@
   networking = {
     hostName = "north";
     networkmanager.enable = true;
-    interfaces.eno1.ipv4.addresses = [{
+    # interfaces.eno1.ipv4.addresses = [{
+    #   address = "192.168.31.200";
+    #   prefixLength = 24;
+    # }];
+    interfaces.wlp9s0.ipv4.addresses = [{
       address = "192.168.31.200";
-      prefixLength = ;
+      prefixLength = 24;
     }];
     defaultGateway = {
       address = "192.168.31.222";
-      interface = "eno1";
+      interface = "wlp9s0";
     };
     nameservers = ["192.168.31.222"];
     firewall = {
