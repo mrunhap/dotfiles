05d3edf842a7f4c89e4c6b1cf2d10b734c401853
mrunhap <liubolovelife@gmail.com>
2024-07-15 19:12:49 +0800

    darwin add unstable packages

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -1,20 +1,34 @@
-{ lib, inputs, nixpkgs, home-manager, darwin, ... }:
+{ lib, inputs, nixpkgs, nixpkgs-unstable, home-manager, darwin, ... }:
 
 let
-  system = "x86_64-darwin";
-in {
-  mac = darwin.lib.darwinSystem {
-    inherit system;
-    specialArgs = {inherit inputs;};
-    modules = [
-      ./configuration.nix
-
-      home-manager.darwinModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.liubo = import ./home.nix;
-      }
-    ];
+  systemConfig = system: {
+    system = system;
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    upkgs = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
   };
+in {
+  mac =
+    let
+      inherit (systemConfig "x86_64-darwin") system pkgs upkgs;
+    in
+      darwin.lib.darwinSystem {
+        inherit system;
+        specialArgs = { inherit inputs system pkgs upkgs; };
+        modules = [
+          ./configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.liubo = import ./home.nix;
+            home-manager.extraSpecialArgs.upkgs = upkgs;
+          }
+        ];
+      };
 }
